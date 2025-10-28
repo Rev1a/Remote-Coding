5c6a5652e863817e50bf27c33458e12812384fef
awwds <13711028897@163.com>
2025-10-28 13:22:49 +0800

    2025.10.28 13:22

--- C/【入门4】数组/P2615.cpp
+++ C/【入门4】数组/P2615.cpp
@@ -3,5 +3,14 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    
+    int arr[n][n]={0};
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<arr[i][j];
+        }
+        cout<<"\n";
+    }
+    return 0;
 }
