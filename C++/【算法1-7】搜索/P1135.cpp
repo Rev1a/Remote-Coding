ca5a5f9c307911467fa16d34635e5ef293d141a6
Rev1a <vdivercesu@gmail.com>
2025-11-10 12:42:20 +0800

    ACM HYW

--- C++/【算法1-7】搜索/P1135.cpp
+++ C++/【算法1-7】搜索/P1135.cpp
@@ -1,7 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool vis[205];
+vector<int> flr(205);
+int cnt = 0;
+void bfs(int u)
+{
+    if(u==b)
+    {
+
+        cout<<cnt;
+    }
+    for(int i=0;i<n;i++)
+    {
+        
+    }
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n,a,b;
+    cin>>n>>a>>b;
+  
+    for(int i=0;i<n;i++)
+    {
+        cin>>flr[i];
+    }
+    bfs(a);
+    return 0;
 }
---
+++ C++/【算法1-7】搜索/P2404.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+void dfs(int x)
+{
+    if(x>n)
+    {
+            
+    }
+
+
+
+
+}
+int main()
+{
+    cin>>n;
+    dfs(1);
+    return 0;
+}
