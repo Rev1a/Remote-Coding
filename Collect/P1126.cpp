d5843cf6b7bf26199b69f7c4ab50e3ce12308ece
awwds <13711028897@163.com>
2025-11-18 18:10:33 +0800

    ACM

--- Collect/P1126.cpp
+++ Collect/P1126.cpp
@@ -1,50 +1,69 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-const int Maxs = 55;
-int n, m, sx, sy, tx, ty;
-bool mp[Maxs][Maxs];
-bool vis[Maxs][Maxs][4];
-char ts;
-struct R
-{
-    int x,y,s,d;
-};
+int a[55][55];
+int map[55][55];
+bool vis[55][55][5];   //1  2  3  4 
+                       //东 南 西 北
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
     {
-        for (int j = 1; j <= m; j++)
-        {
-            int t;
-            cin >> t;
-            while (t)
+        for(int j=1;j<=m;j++)
+        {   
+            int c;
+            cin>>c;
+            if(c==1)
             {
-                mp[i][j] = mp[i - 1][j] = mp[i][j - 1] = mp[i - 1][j - 1];
+                a[i][j] = 1;
+                a[i+1][j] = 1;
+                a[i+1][j+1] = 1;
+                a[i][j+1] = 1;
+            }
+            else
+            {
+                a[i][j] = 0;
             }
         }
     }
-    queue<R> Q;
-    cin>>sx>>sy>>tx>>ty>>ts;
-    R t1;
-    t1.x = sx;
-    t1.y = sy;
-    t1.s = 0;
-    if(ts = 'N')t1.d = 0;
-    if(ts = 'E')t1.d = 1;
-    if(ts = 'S')t1.d = 2;
-    if(ts = 'W')t1.d = 3; 
-    Q.push(t1);
-    while(!Q.empty())
-    {
-        t1 = Q.front();
-        vis[t1.x][t1.y][t1.d] = 1;
-        if(t1.x == tx && t1.y == ty)
-        {
-            cout<<t1.s;
-        }
-    }
-    return 0;
-}
+    int bx,by,ex,ey;
+    char cha;
+    cin>>bx>>by>>ex>>ey>>cha;
+    int dire = 0;
+    if(cha == 'E')dire = 1;
+    if(cha == 'S')dire = 2;
+    if(cha == 'W')dire = 3;
+    if(cha == 'N')dire = 4;
+    vis[bx][by][dire] = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+} 
