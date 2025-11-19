3705cac190fdc0d5932d08caf349fe1811d3d9d7
awwds <13711028897@163.com>
2025-11-19 13:04:50 +0800

    FUCK ACM

--- C++/【算法1-7】搜索/P2404.cpp
+++ C++/【算法1-7】搜索/P2404.cpp
@@ -1,16 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n;
-void dfs(int x)
+int q;
+vector<int> path;
+void dfs(int r,int l)
 {
-    if(x>n)
+    if(r==0)
     {
-            
+        for(int i=0;i<path.size();i++)
+        {
+            if(i) cout<<"+";
+            cout<<path[i];
+        }
+        cout<<'\n';
+        return;
+    }
+    for(int i=l;i<=r;i++)
+    {   
+        if(i==q)continue;
+        path.push_back(i);
+        dfs(r-i,i);
+        path.pop_back();
     }
 }
 int main()
 {
-    cin>>n;
-    dfs(1);
+    cin>>q;
+    //cerr << "DEBUG READ q=" << q << endl;
+    dfs(q,1);
     return 0;
 }
