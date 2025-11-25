b34b3cadecb0436ee926d37e15fd7c2600065788
Rev1a <vdivercesu@gmail.com>
2025-11-25 09:19:05 +0800

    1424

---
+++ C++/【算法1-6】二分查找与二分答案/P1678.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int m,n;
+    vector<int> a(m),b(n);
+    for(int i=0;i<m;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        cin>>b[i];
+    }
+    
+}
--- Collect/P8784.cpp
+++ Collect/P8784.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int MOD = 1e9 + 7;
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n;
-    const int MOD = 1e9 + 7;
+    cin>>n;
+    
 }
